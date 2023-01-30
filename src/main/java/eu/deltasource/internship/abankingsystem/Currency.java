b70a796e60a6bd9e499712070db938abc6843051
Evgeni-Georgiev <evgenigg2000@gmal.com>
2023-01-30 17:59:37 +0200

    Added enumerations for taxes and exchange rates.

---
+++ src/main/java/eu/deltasource/internship/abankingsystem/Currency.java
@@ -0,0 +1,14 @@
+package eu.deltasource.internship.abankingsystem;
+
+public enum Currency {
+
+    BGNEUR,
+    BGNUSD,
+    USDBGN,
+    USDEUR,
+    EURBGN,
+    EURUSD,
+    EUREUR,
+    BGNBGN,
+    USDUSD
+}
---
+++ src/main/java/eu/deltasource/internship/abankingsystem/Taxes.java
@@ -0,0 +1,6 @@
+package eu.deltasource.internship.abankingsystem;
+
+public enum Taxes {
+    TAX_TO_THE_SAME_BANK,
+    TAX_TO_DIFFERENT_BANK
+}

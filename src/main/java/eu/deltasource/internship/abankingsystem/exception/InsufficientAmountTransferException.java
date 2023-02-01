0a842c9fcd5d28e903fa886208a3517f7a2c7016
Evgeni-Georgiev <evgenigg2000@gmal.com>
2023-02-01 13:25:48 +0200

    TODO

--- src/main/java/eu/deltasource/internship/abankingsystem/exception/InsufficientAmountTransferException.java
+++ src/main/java/eu/deltasource/internship/abankingsystem/exception/InsufficientAmountTransferException.java
@@ -1,6 +1,8 @@
 package eu.deltasource.internship.abankingsystem.exception;
 
 public class InsufficientAmountTransferException extends Exception{
+
+    // TODO: Add message templates
     public InsufficientAmountTransferException (String str) {
         super(str);
     }

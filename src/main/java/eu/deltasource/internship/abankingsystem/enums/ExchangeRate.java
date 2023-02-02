1ddf6a643914c71ace5c687ff6cea708b0c55629
Evgeni-Georgiev <evgenigg2000@gmal.com>
2023-02-02 08:53:13 +0200

    Added Exchange rates as enum.

---
+++ src/main/java/eu/deltasource/internship/abankingsystem/enums/ExchangeRate.java
@@ -0,0 +1,14 @@
+package eu.deltasource.internship.abankingsystem.enums;
+
+public enum ExchangeRate {
+
+    BGNEUR,
+    BGNUSD,
+    USDBGN,
+    USDEUR,
+    EURBGN,
+    EURUSD,
+    EUREUR,
+    BGNBGN,
+    USDUSD
+}
